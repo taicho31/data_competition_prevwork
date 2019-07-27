445a259bf63099df6f80901cd7f7ece507901ed6
taichi_on_macbookair <tnakamura493@gmail.com>
2019-07-27 22:46:49 +0900

    mod in implementation shell

--- implementation.sh
+++ implementation.sh
@@ -5,13 +5,13 @@ TARGET=$(<feature_setting/target_feature.txt)
 echo "$ID"
 echo "$TARGET"
 
-#ls ../input/*.feather > /dev/null 2>&1
-#if [ $? -ne 0 ]; then
-#    echo "original feather file exists"
-#else
-#    python3 memory_reduce.py "$ID" "$TARGET"
-#fi
+ls ../input/*.feather > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo "original feather file exists"
+else
+    python3 memory_reduce.py "$ID" "$TARGET"
+fi
 
 python3 eda.py "$ID" "$TARGET"
-#python3 feature_engineering.py "$ID" "$TARGET"
-#python3 model/lgb_train.py $ID $TARGET
+python3 feature_engineering.py "$ID" "$TARGET"
+python3 model/lgb_train.py $ID $TARGET
