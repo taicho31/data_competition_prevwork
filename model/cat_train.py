d054f576ab0749e57f235cf7cd0f8424ad5c2a40
taichi_on_macbookair <tnakamura493@gmail.com>
2019-09-30 11:11:09 +0900

    modify cat boost learning process

--- model/cat_train.py
+++ model/cat_train.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import feather
-from sklearn.metrics import roc_auc_score, roc_curve, mean_squared_error
+from sklearn.metrics import confusion_matrix, log_loss, classification_report, accuracy_score, roc_auc_score, roc_curve, mean_squared_error
 from sklearn.model_selection import StratifiedKFold
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
 from skopt import BayesSearchCV
@@ -51,6 +51,10 @@ features = [c for c in train.columns if c not in [id_feature, target_feature]]
 target= train[target_feature]
 logger.info('data install complete')
 
+logger.info('----------Learning start----------')
+CLASS = 9
+n_folds = 5
+
 params = {
     "eval_metric": "AUC",
     "objective": "Logloss",
@@ -62,86 +66,59 @@ params = {
     "max_depth": 10,
     #"rsm": 0.1,
     "scale_pos_weight": 5,
-#"boosting": "Ordered"
+    #"boosting": "Ordered"
 }
 
-#logger.info('Paramter tuning by BayesSearch')
-#params = {'eval_metric':"AUC", 'objective':"Logloss", 'early_stopping_rounds':1000}
-#bayes_cv_tuner = BayesSearchCV(
-#            estimator = CatBoostClassifier(eval_metric="AUC", objective="Logloss", early_stopping_rounds=1000),
-#            search_spaces = {
-#                            'learning_rate': (0.01, 0.1),
-#                            'iterations': (10000, 50000),
-#                            'subsample': (0.6, 1.0),
-#                            'colsample_bylevel': (0.01,0.1),
-#                            'max_depth': (30, 50),
-#                            'depth':(6, 16),
-#                            'rsm':(0.1, 0.99),
-#                            'scale_pos_weight': (1,10),
-#                            'boosting': ["Ordered", "Plain"],
-#                            },
-#            scoring = "roc_auc",
-#            cv = StratifiedKFold(n_splits=3, shuffle=True, random_state=42),
-#            n_jobs = -3,
-#            n_iter = 10,
-#            verbose = 0,
-#            refit = True,
-#            random_state = 42
-#            )
-
-#result = bayes_cv_tuner.fit(train[selected_features].values, target.values, callback=status_print)
-#logger.info('found parameters by bayes searchCV: {}'.format(bayes_cv_tuner.best_params_))
-#logger.info('best scores by bayes searchCV: {}'.format(bayes_cv_tuner.best_score_))
-
-#params.update(bayes_cv_tuner.best_params_)
-
-#path = "../result/parameter_catboost.csv"
-#keys = pd.DataFrame(list(my_dict.keys()))
-#values = pd.DataFrame(list(my_dict.values()))
-#current = pd.concat([keys, values], axis=1)
-#current.columns = [str(start_time)+"keys", str(start_time)+"values"]
-#if os.path.isfile(path):
-#    data = pd.read_csv(path)
-#   data = pd.concat([data, current], axis=1)
-#    data.to_csv(path)
-#else:
-#    current.to_csv(path)
-
-logger.info('Learning start')
-folds = StratifiedKFold(n_splits=2, shuffle=False, random_state=44000)
-oof = train[[id_feature, target_feature]]
-oof['predict'] = 0
+folds = StratifiedKFold(n_splits=n_folds, shuffle=False, random_state=44000)
+oof = np.zeros((len(train_df),CLASS))
 predictions = pd.DataFrame(test[id_feature])
+val_scores = []
+feature_importance_df = pd.DataFrame()
+feature_importance_df["feature"] = features
+yp = np.zeros((test.shape[0] ,CLASS))
 
 for fold_, (trn_idx, val_idx) in enumerate(folds.split(train.values, target.values)):
-    logger.info('Fold {}'.format(fold_+1))
+    print('Fold {}'.format(fold_+1))
     cat = CatBoostClassifier(**params)
     cat.fit(train.iloc[trn_idx][features], target.iloc[trn_idx])
-    oof['predict'][val_idx] = cat.predict_proba(train.iloc[val_idx][features])[:,1]
-
-    predictions["Fold_"+str(fold_+1)] = cat.predict_proba(test[features])[:,1]
-    logger.debug('CV score: {:<8.5f}'.format(roc_auc_score(target.iloc[val_idx], oof["predict"][val_idx])))
+    
+    valid_result = xgb_model.predict_proba(train.iloc[val_idx][features])
+    yp += xgb_model.predict_proba(test[features]) / n_folds
+    
+    oof[val_idx] = valid_result
+    val_score = roc_auc_score(target[val_idx], np.argmax(valid_result, axis=1))
+    val_scores.append(val_score)
+    feature_importance_df["importance_fold"+str(i)] = xgb_model.feature_importances_
 
 logger.info('Learning end')
-score = roc_auc_score(target, oof["predict"])
 
-predictions["Result"] = np.mean(predictions.iloc[:,2:], axis=1)
+logger.info('-------Performance check and prediction-------')
+mean_score = np.mean(val_scores)
+std_score = np.std(val_scores)
 
-logger.info('record oof')
-path = "../result/catboost_oof.csv"
-if os.path.isfile(path):
-    data = pd.read_csv(path)
-else:
-    data = pd.DataFrame()
-data[str(start_time)+str(target_feature)] = oof["predict"]
-data.to_csv(path, index=None)
+oof_prediction = np.argmax(oof, axis=1)
+all_score = roc_auc_score(target, oof_prediction)
+logger.debug("Mean score: %.9f, std: %.9f. All score: %.9f." % (mean_score, std_score, all_score))
+print(confusion_matrix(target, oof_prediction))
+print(classification_report(target, oof_prediction))
 
-logger.info('make submission file')
+predictions[target_feature] = np.argmax(yp, axis=1)
+
+#logger.info('--------record oof contents--------')
+#path = "../result/catboost_oof.csv"
+#if os.path.isfile(path):
+#    data = pd.read_csv(path)
+#else:
+#    data = pd.DataFrame()
+#data[str(start_time)+str(target_feature)] = oof["predict"]
+#data.to_csv(path, index=None)
+
+logger.info('--------make submission file---------')
 sub_df = pd.DataFrame({str(id_feature):test[id_feature].values})
 sub_df[target_feature] = predictions["Result"]
 sub_df.to_csv("../result/submission_cat_"+str(score)+".csv", index=False)
 
-logger.info('record submission contents')
+logger.info('--------record submission contents--------')
 path = "../result/catboost_submission_sofar.csv"
 if os.path.isfile(path):
     data = pd.read_csv(path)
