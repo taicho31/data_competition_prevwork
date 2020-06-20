bfa70360534672d4fd779c3020deee0a27ad11a0
taichi_on_macbookair <tnakamura493@gmail.com>
2020-06-20 16:34:17 +0900

    add focal_loss

---
+++ common_util/focal_loss.py
@@ -0,0 +1,32 @@
+# https://www.kaggle.com/c/ieee-fraud-detection/discussion/104603
+def robust_pow(num_base, num_pow):
+    # numpy does not permit negative numbers to fractional power
+    # use this to perform the power algorithmic
+
+    return np.sign(num_base) * (np.abs(num_base)) ** (num_pow)
+
+def focal_binary_object(pred, dtrain):
+    gamma_indct = 2.5
+    # retrieve data from dtrain matrix
+    label = dtrain.get_label()
+    # compute the prediction with sigmoid
+    sigmoid_pred = 1.0 / (1.0 + np.exp(-pred))
+    # gradient
+    # complex gradient with different parts
+    g1 = sigmoid_pred * (1 - sigmoid_pred)
+    g2 = label + ((-1) ** label) * sigmoid_pred
+    g3 = sigmoid_pred + label - 1
+    g4 = 1 - label - ((-1) ** label) * sigmoid_pred
+    g5 = label + ((-1) ** label) * sigmoid_pred
+    # combine the gradient
+    grad = gamma_indct * g3 * robust_pow(g2, gamma_indct) * np.log(g4 + 1e-9) + \
+           ((-1) ** label) * robust_pow(g5, (gamma_indct + 1))
+    # combine the gradient parts to get hessian components
+    hess_1 = robust_pow(g2, gamma_indct) + \
+             gamma_indct * ((-1) ** label) * g3 * robust_pow(g2, (gamma_indct - 1))
+    hess_2 = ((-1) ** label) * g3 * robust_pow(g2, gamma_indct) / g4
+    # get the final 2nd order derivative
+    hess = ((hess_1 * np.log(g4 + 1e-9) - hess_2) * gamma_indct +
+            (gamma_indct + 1) * robust_pow(g5, gamma_indct)) * g1
+
+    return grad, hess
