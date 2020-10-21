3a996299a151c623d3c9912c3320dd25886e4ad4
taichi_on_macbookair <tnakamura493@gmail.com>
2020-10-21 12:42:26 +0900

    lgb + adaptive lr

--- common_util/focal_loss.py
+++ common_util/focal_loss.py
@@ -1,6 +1,8 @@
 # https://www.kaggle.com/c/ieee-fraud-detection/discussion/104603
 #LiLightGbm USE : lgb.train(fobj=focal_binary_object)
+ and feval needs to be set as well
 #Xgboost USE : xgb.train(obj =focal_binary_object)
+
 
 def robust_pow(num_base, num_pow):
     # numpy does not permit negative numbers to fractional power
