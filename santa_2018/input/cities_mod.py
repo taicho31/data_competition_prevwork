2bd98c541c5fdc35f902c7e96cd7014b8c7ab1ce
taichi_on_macbookair <tnakamura493@gmail.com>
2020-11-22 21:30:40 +0900

    move santa_2018 data

--- santa_2018/input/cities_mod.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-city_mod = pd.read_csv("cities_mod.csv")
-city_mod["X"] = city_mod["X"] *1000
-city_mod["Y"] = city_mod["Y"] * 1000
-city_mod.to_csv("cities_mod.csv", index=False)
