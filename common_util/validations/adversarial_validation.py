8ff2401aaba0febcc8e9bf81c973199eede05785
taichi_on_macbookair <tnakamura493@gmail.com>
2020-11-23 13:09:14 +0900

    modify code

--- common_util/validations/adversarial_validation.py
+++ common_util/validations/adversarial_validation.py
@@ -1,22 +1,22 @@
+# preprocess need to be done in advance
+from sklearn.model_selection import KFold
+import datetime
+import lightgbm as lgb
+
 def adversrial_validation(tr, te):
-    from sklearn.model_selection import KFold
+    train_only_feat = list(set(train.columns) - set(test.columns))
+    test_only_feat= list(set(test.columns) - set(train.columns))
+    
+    tr.drop(train_only_feat, axis=1, inplace=True)
+    te.drop(test_only_feat, axis=1, inplace=True)
+    
     tr['ad_target'] = 1
     te['ad_target'] = 0
     n_train = tr.shape[0]
     ad_df = pd.concat([tr, te], axis = 0)
-    ad_df = ad_df.iloc[np.random.permutation(len(ad_df))]
-    ad_df.reset_index(drop = True, inplace = True)
-
-    i_d = "chip_id"
-    prev_target = "target"
-    # Remove columns with only one value in our training set
-    predictors = list(ad_df.columns.difference([i_d, prev_target])) # variables to use prediction
-    df_train = ad_df.iloc[:n_train].copy()
-    cols_to_remove = [c for c in predictors if df_train[c].nunique() == 1 and c != "ad_target"]
-    ad_df.drop(cols_to_remove, axis=1, inplace=True)
     
     # Update column names
-    predictors = [c for c in predictors if c != "ad_target"]
+    predictors = [c for c in ad_df.columns if c != "ad_target"]
     print(predictors)
     
     NFOLD = 5
@@ -24,20 +24,21 @@ def adversrial_validation(tr, te):
     ad_df.reset_index(drop = True, inplace = True)
     
     # lgb params
-    lgb_params = {'num_leaves': 50,
-         'min_data_in_leaf': 30, 
+    lgb_params = {
          'objective':'binary',
          'max_depth': 5,
          'learning_rate': 0.2,
-         "min_child_samples": 20,
-         "boosting": "gbdt",
-         "feature_fraction": 0.9,
-         "bagging_freq": 1,
-         "bagging_fraction": 0.9 ,
-         "bagging_seed": 44,
          "metric": 'auc',
-         "verbosity": -1}
-
+        }
+         #"min_child_samples": 20,
+         #"boosting": "gbdt",
+         #"feature_fraction": 0.9,
+         #"bagging_freq": 1,
+         #"bagging_fraction": 0.9 ,
+         #"bagging_seed": 44,
+         #'num_leaves': 50,
+         #'min_data_in_leaf': 30, 
+    
     # Get folds for k-fold CV
     folds = KFold(n_splits = NFOLD, shuffle = True, random_state = 0)
     fold = folds.split(ad_df)
@@ -46,7 +47,6 @@ def adversrial_validation(tr, te):
     target = 'ad_target'
     
     eval_score = 0
-    n_estimators = 0
     eval_preds = np.zeros(ad_df.shape[0])
 
     for i, (train_index, test_index) in enumerate(fold):
@@ -69,13 +69,12 @@ def adversrial_validation(tr, te):
                          valid_names = ['train', 'valid'], 
                          evals_result = eval_results, 
                          num_boost_round = 5000,
-                         early_stopping_rounds = 100,
-                         verbose_eval = 100)
+                         early_stopping_rounds = 10,
+                         verbose_eval = 10)
     
         print("\nRounds:", bst.best_iteration)
         print("AUC: ", eval_results['valid']['auc'][bst.best_iteration-1])
     
-        n_estimators += bst.best_iteration
         eval_score += eval_results['valid']['auc'][bst.best_iteration-1]
    
         eval_preds[test_index] += bst.predict(valid_X, num_iteration = bst.best_iteration)
@@ -87,11 +86,9 @@ def adversrial_validation(tr, te):
     feature_importance_df["Average"] = np.mean(feature_importance_df.iloc[:,1:NFOLD+1], axis=1)
     feature_importance_df["Std"] = np.std(feature_importance_df.iloc[:,1:NFOLD+1], axis=1)
     feature_importance_df["Cv"] = feature_importance_df["Std"] / feature_importance_df["Average"]
-    n_estimators = int(round(n_estimators/NFOLD,0))
     eval_score = round(eval_score/NFOLD,6)
 
     print("\nModel Report")
-    print("Rounds: ", n_estimators)
     print("AUC: ", eval_score) 
     
     lgb.plot_importance(bst, max_num_features = 20)
@@ -110,6 +107,8 @@ def adversrial_validation(tr, te):
 
     #because 0 is assigned to test data, pred close to 0 means that the data is similar to test data
     #print(df_av_train.head(20)) 
+    
     return feature_importance_df
-sam = adversrial_validation(new_train, new_test)
+
+sam = adversrial_validation(train, test)
 sam.sort_values("Value1", ascending=False).reset_index(drop=True)
